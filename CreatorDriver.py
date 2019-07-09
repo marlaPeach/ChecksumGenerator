495fcfa2585a7e56d1b7eeb754e76bbd03b9eab4
Tiffany Schmidt <schmidttl@cmog.org>
2019-07-09 12:19:56 -0400

    Driver for the checksum generator program.

---
+++ CreatorDriver.py
@@ -0,0 +1,13 @@
+#Driver for checksum manifest maker.
+
+import UserInterface
+import ChecksumGenerator
+
+
+
+#main
+def main():
+    UI = UserInterface.Window()
+
+if __name__ == '__main__':
+    main()
